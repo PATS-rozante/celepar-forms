851b4a8cd4beffc51c599956bae55f0423572afb
Alexandre Rozante <alexrozante@hotmail.com>
2025-10-03 19:25:02 -0300

    Alexandre - nova lib de separators

--- POC/Amostra 05/gen-banner.bat
+++ POC/Amostra 05/gen-banner.bat
@@ -1,4 +1,4 @@
 @echo off
-del /Q .\out\Amostra_05_BANNER*.* >nul >>nul
-call DO Merge -form ./rhc008.xatw -filter rhc008.xfilter -data ./p.rhc.amostra5-BANNER.txt -config $U/Config/CeleparPDF.prt -output ./out/Amostra_05_BANNER.pdf -logfile ./out/Amostra_05_BANNER_pdf.log
-dir .\out\Amostra_05_BANNER*.*
+del /Q .\out\Amostra_05_full*.* >nul >>nul
+call DO Merge -form ./rhc008.xatw -filter rhc008.xfilter -data ./XPR00012_RHCEOS01_PRHC_30_09_2025_16_23_10.bin -config $U/Config/CeleparPDF.prt -output ./out/Amostra_05_full.pdf -logfile ./out/Amostra_05_full.log
+dir .\out\Amostra_05_full*.*
